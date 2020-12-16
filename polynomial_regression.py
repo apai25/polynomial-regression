1c4aba1bb23f94b919b45a2e4f1d12a4a42b90b9
Anirudh Pai <anirudhnpai@gmail.com>
2020-12-15 23:38:05 -0800

    added non-smooth graphing for comparison

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -33,6 +33,25 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_poly_train, y_train)
 
+"""
+# Non-smooth curve graphing with the training data
+plt.scatter(x_train, y_train, color='red', label='Training Data Points')
+plt.plot(x_train, regressor.predict(x_poly_train), color='blue', label='Model Curve')
+plt.title('Job Level vs Salary (Training Dataset)')
+plt.xlabel('Job Level (1 - 10)')
+plt.ylabel('Salary')
+plt.legend()
+plt.show()
+
+# Non-smooth curve graphing with the test data
+plt.scatter(x_test, y_test, color='red', label='Test Data Points')
+plt.plot(x_test, regressor.predict(x_poly_test), color='blue', label='Model Curve')
+plt.title('Job Level vs Salary (Test Dataset)')
+plt.xlabel('Job Level (1 - 10)')
+plt.ylabel('Salary')
+plt.legend()
+plt.show()
+"""
 # Creating x_grid datasets (have smaller increments) for smoother graphing
 x_grid_train = np.arange(min(x_train), max(x_train) + .01, step=0.01)
 x_grid_train = x_grid_train.reshape(len(x_grid_train), 1)
@@ -40,7 +59,7 @@ x_grid_train = x_grid_train.reshape(len(x_grid_train), 1)
 x_grid_test = np.arange(min(x_test), max(x_test) + .01, step=0.01)
 x_grid_test = x_grid_test.reshape(len(x_grid_test), 1)
 
-# Graphing the model with the training data
+# Smooth curve graphing with the training data
 plt.scatter(x_train, y_train, color='red', label='Training Data Points')
 plt.plot(x_grid_train, regressor.predict(poly_transform.transform(x_grid_train)), color='blue', label='Model Curve')
 plt.title('Job Level vs Salary (Training Dataset)')
@@ -49,7 +68,7 @@ plt.ylabel('Salary')
 plt.legend()
 plt.show()
 
-# Graphing the model with the testing data
+# Smooth curve graphing with the test data
 plt.scatter(x_test, y_test, color='red', label='Test Data Points')
 plt.plot(x_grid_test, regressor.predict(poly_transform.transform(x_grid_test)), color='blue', label='Model Curve')
 plt.title('Job Level vs Salary (Test Dataset)')
